38902cd3019ba0416c841c1925e2246c7abd9e9d
ZenFoe <133615878+ZenFoe@users.noreply.github.com>
2024-11-06 13:42:06 -0500

    Update surf.js

--- pages/surf.js
+++ pages/surf.js
@@ -1,4 +1,23 @@
-router.push(`/browse/${browse.toString()}`);
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function Surf() {
+    const [url, setURL] = useState("");
+    const router = useRouter();
+
+    const handle = (e) => {
+        e.preventDefault();
+
+        const browse = !url.startsWith("http") ? `https://${url}` : url;
+
+        try {
+            new URL(browse);
+        } catch {
+            return alert(url.length ? "Invalid url!" : "Enter a url!");
+        }
+
+        e.target.reset();
+        window.location.href = `https://www.nebulaofficial.gq/proxy/${btoa(browse.toString())}`;
     }
 
     return (
